ec16be7483b8223ccc2416f59232ad2e2fdf14bd
hector <hectormobravo@gmail.com>
2018-10-27 16:49:19 -0700

    improved layout, added resume and matched site with resume narrative

--- app/controllers/gusi_controller.rb
+++ app/controllers/gusi_controller.rb
@@ -2,4 +2,9 @@ class GusiController < ApplicationController
 	def homepage
 		@page_function  = Page.where(function: params[:page]).take.function if params[:page]
 	end
+
+
+	def hectormb_resume
+	end
+
 end
